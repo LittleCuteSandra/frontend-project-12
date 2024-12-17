ade6c53ca62c74088f06c5c14a1529e0d69925c1
Королева Александра <sasha.sasha.koroleva@mail.ru>
2024-12-17 16:29:45 +0300

    change frontend/src/utils/formValidation.js

--- frontend/src/utils/formValidation.js
+++ frontend/src/utils/formValidation.js
@@ -1,6 +1,6 @@
 import * as Yup from 'yup';
 
-const validationFormSchema = () => Yup.object().shape({
+const validationFormSchema = Yup.object().shape({
   username: Yup.string().required('Обязательное поле').min(3, 'Минимум 3 буквы').max(20, 'Максимум 20 букв'),
   password: Yup.string().required('Обязательное поле').min(6, 'Минимум 6 символов'),
   confirmPassword: Yup.string().required('Обязательное поле').oneOf([Yup.ref('password'), null], 'Пароли должны совпадать'),
